d825f256cc235db8f761424bcacd937cf4d78a7c
danielshires <hello@danielshires.com>
2022-06-14 18:53:26 +1000

    Assignmwnent complete

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,39 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+
+    constructor(props){
+        super()
+        this.state = {
+           secondsLeft: props.initialCount
+        }
+    }
+
+    reduceCount = () => {
+        this.setState({
+            secondsLeft: this.state.secondsLeft - 1 
+        })
+    }
+
+    countDown = () => {
+        console.log(this.state.secondsLeft)
+        const currentState = this.state.secondsLeft
+        // setInterval(this.reduceCount, 1000)
+        if(currentState > 0){
+            return `${this.state.secondsLeft} seconds left before I go boom!`
+        } else {
+            return `Boom!`
+        }
+      
+
+    }
+    
+    render(){
+        console.log(this.props.initialCount)
+        console.log(this.state.secondsLeft)
+        return(
+            <div initialCount={this.state.secondsLeft}>{this.countDown()}</div>
+        )
+    }
+}
